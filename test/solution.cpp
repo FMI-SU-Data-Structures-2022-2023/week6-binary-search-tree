9196f84cc5c1d5c9b8aac2502a1785bd371527b3
svilenbs@gmail.com <svilenbs@gmail.com>
2022-11-16 17:00:49 +0200

    Fix UTs

--- test/solution.cpp
+++ test/solution.cpp
@@ -20,7 +20,7 @@ TEST_CASE("Validate isContains") {
     CHECK(isContains(test, 140) == true);
     CHECK(isContains(test, 25) == true);
     CHECK(isContains(test, 0) == false);
-    CHECK(isContains(test, 150) == true);
+    CHECK(isContains(test, 150) == flase);
     clean(test);
 }
 
@@ -44,9 +44,9 @@ TEST_CASE("Validate getMax") {
     test->left = new Node(50);
     test->right = new Node(140);
     test->left->left = new Node(25);
-    CHECK(getMin(test)->key == 140);
+    CHECK(getMax(test)->key == 140);
     test->right->right = new Node(180);
-    CHECK(getMin(test)->key == 180);
+    CHECK(getMax(test)->key == 180);
 
     clean(test);
 }
